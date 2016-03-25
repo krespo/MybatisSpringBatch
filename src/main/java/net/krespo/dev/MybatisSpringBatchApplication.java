f72aac73471c05cf0828b6c43b34060a070ca92d
louis.jeon <louis.jeon@daumkakao.com>
2016-03-25 11:28:48 +0900

    first commit

---
+++ src/main/java/net/krespo/dev/MybatisSpringBatchApplication.java
@@ -0,0 +1,12 @@
+package net.krespo.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisSpringBatchApplication.class, args);
+	}
+}
---
+++ src/test/java/net/krespo/dev/MybatisSpringBatchApplicationTests.java
@@ -0,0 +1,16 @@
+package net.krespo.dev;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MybatisSpringBatchApplication.class)
+public class MybatisSpringBatchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
